47a1668d74a48f3312448d0061cc93af39ec29df
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-10-13 22:41:46 +0200

    Lesson exception try -catch

--- src/Test.java
+++ src/Test.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj liczbę");
-        int number = 0;
+        int [] numbers = new int[2];
         boolean error = true;
 
         do {
             try {
-                number = sc.nextInt();
+                System.out.println("Podaj 1 liczbę");
+                numbers[0] = sc.nextInt();
+
+                System.out.println("Podaj 2 liczbę");
+                numbers[0] = sc.nextInt();
+
+                System.out.println("Którą liczbę chcesz wyświetlić. 1 czy 2");
+                System.out.println("Liczba" + numbers[sc.nextInt() - 1]);
                 error = false;
-                System.out.println("Wczytano poprawną liczbę");
-            } catch (InputMismatchException ex) {
-                System.err.println("Podana wartość nie jest liczbą całkowitą");
+
+            } catch (InputMismatchException e)  {
+                System.err.println("Podana wartość nie jest liczbą całkowitą. Spróbuj ponownie");
                 sc.nextLine();
-                //ex.printStackTrace(); //metoda z wskazaniem szczegółów błędu;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Miało być 1 lub 2. Spróbuj ponownie");
+                sc.nextLine();
             }
         } while (error);
 
-
-        System.out.println("Podałeś liczbę " + number);
         sc.close();
-
     }
 }
